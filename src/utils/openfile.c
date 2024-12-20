31a5da9b8ca816d759b6b3f06bd20e5418290392
qgymib <4520263+qgymib@users.noreply.github.com>
2024-12-20 23:41:00 +0800

    Fix build error

--- src/utils/openfile.c
+++ src/utils/openfile.c
@@ -6,36 +6,44 @@
 #include "utils/defines.h"
 #include "openfile.h"
 
-typedef struct csplice_file_impl
-{
-    csplice_file_t basis;
-    intptr_t       handle;
-} csplice_file_impl_t;
-
 #if defined(_WIN32)
 
 #include <windows.h>
 
+typedef struct csplice_file_impl
+{
+    csplice_file_t basis;
+    HANDLE         handle;
+} csplice_file_impl_t;
+
+typedef struct open_mode_map
+{
+    const char *mode;
+    DWORD       access;
+    DWORD       attributes;
+    DWORD       disposition;
+} open_mode_map_t;
+
 static void _csplice_openfile_close(struct csplice_file *thiz)
 {
     csplice_file_impl_t *impl = container_of(thiz, csplice_file_impl_t, basis);
-    if ((HANDLE)impl->handle != INVALID_HANDLE_VALUE)
+    if (impl->handle != INVALID_HANDLE_VALUE)
     {
-        CloseHandle((HANDLE)impl->handle);
-        impl->handle = (intptr_t)INVALID_HANDLE_VALUE;
+        CloseHandle(impl->handle);
+        impl->handle = INVALID_HANDLE_VALUE;
     }
 }
 
 static int64_t _csplice_openfile_read(struct csplice_file* thiz, void* buff, size_t size)
 {
     csplice_file_impl_t *impl = container_of(thiz, csplice_file_impl_t, basis);
-    if ((HANDLE)impl->handle == INVALID_HANDLE_VALUE)
+    if (impl->handle == INVALID_HANDLE_VALUE)
     {
         return -EINVAL;
     }
 
     DWORD read_sz = 0;
-    if (ReadFile((HANDLE)impl->handle, buff, (DWORD)size, &read_sz, NULL))
+    if (ReadFile(impl->handle, buff, (DWORD)size, &read_sz, NULL))
     {
         return read_sz;
     }
@@ -46,28 +54,71 @@ static int64_t _csplice_openfile_read(struct csplice_file* thiz, void* buff, siz
 static int64_t _csplice_openfile_write(struct csplice_file* thiz, const void* buff, size_t size)
 {
     csplice_file_impl_t *impl = container_of(thiz, csplice_file_impl_t, basis);
-    if ((HANDLE)impl->handle == INVALID_HANDLE_VALUE)
+    if (impl->handle == INVALID_HANDLE_VALUE)
     {
         return -EINVAL;
     }
 
     DWORD write_sz = 0;
-    if (WriteFile((HANDLE)impl->handle, buff, (DWORD)size, &write_sz, NULL))
+    if (WriteFile(impl->handle, buff, (DWORD)size, &write_sz, NULL))
     {
         return write_sz;
     }
     return -EPIPE;
 }
 
-static int _fopen_s(intptr_t* h, const char* path, const char* mode)
+static int wrap_fopen_s(HANDLE *h, const char *path, const char *mode)
 {
+    const DWORD           access_append = (FILE_GENERIC_WRITE & (~FILE_WRITE_DATA)) | FILE_APPEND_DATA;
+    const open_mode_map_t s_mode_map[] = {
+        { "r",  FILE_GENERIC_READ,                      FILE_ATTRIBUTE_READONLY, OPEN_EXISTING },
+        { "r+", FILE_GENERIC_READ | FILE_GENERIC_WRITE, 0,                       OPEN_EXISTING },
+        { "w",  FILE_GENERIC_WRITE,                     0,                       CREATE_ALWAYS },
+        { "w+", FILE_GENERIC_READ | FILE_GENERIC_WRITE, 0,                       CREATE_ALWAYS },
+        { "a",  access_append,                          0,                       OPEN_ALWAYS   },
+        { "a+", FILE_GENERIC_READ | access_append,      0,                       OPEN_ALWAYS   },
+    };
 
+    const open_mode_map_t *token = NULL;
+    for (size_t i = 0; i < ARRAY_SIZE(s_mode_map); i++)
+    {
+        if (strcmp(mode, s_mode_map[i].mode) == 0)
+        {
+            token = &s_mode_map[i];
+            break;
+        }
+    }
+    if (token == NULL)
+    {
+        return -EINVAL;
+    }
+
+    const DWORD share_flag = FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE;
+    *h = CreateFileA(path, token->access, share_flag, NULL, token->disposition,
+        token->attributes, NULL);
+    if (*h == INVALID_HANDLE_VALUE)
+    {
+        const DWORD err = GetLastError();
+        if (err == ERROR_FILE_EXISTS)
+        {
+            return -EEXIST;
+        }
+        return -EINVAL;
+    }
+
+    return 0;
 }
 
 #else
 
 #include <unistd.h>
 #include <fcntl.h>
+
+typedef struct csplice_file_impl
+{
+    csplice_file_t basis;
+    int            handle;
+} csplice_file_impl_t;
 
 typedef struct open_mode_map
 {
@@ -107,7 +158,7 @@ static int64_t _csplice_openfile_write(struct csplice_file *thiz, const void *bu
     return write_sz;
 }
 
-static int _fopen_s(intptr_t* h, const char *path, const char *mode)
+static int wrap_fopen_s(intptr_t *h, const char *path, const char *mode)
 {
     static const open_mode_map_t open_map[] = {
         { "r",  O_RDONLY                    },
@@ -160,11 +211,11 @@ int csplice_openfile(csplice_file_t **file, const char *path, const char *mode)
     impl->basis.read = _csplice_openfile_read;
     impl->basis.write = _csplice_openfile_write;
 
-    int errcode;
-    if ((errcode = _fopen_s(&impl->handle, path, mode)) < 0)
+    const int ret = wrap_fopen_s(&impl->handle, path, mode);
+    if (ret < 0)
     {
         free(impl);
-        return errcode;
+        return ret;
     }
 
     *file = &impl->basis;
